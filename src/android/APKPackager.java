61cafecb9e882b33ff3e414278792e4d83942ed8
Andrew Grieve <agrieve@chromium.org>
2014-09-26 16:20:28 -0400

    Changes to make it work within CADT

--- src/android/APKPackager.java
+++ src/android/APKPackager.java
@@ -23,6 +23,9 @@ import java.security.PrivateKey;
 import java.security.Security;
 import java.security.cert.X509Certificate;
 import kellinwood.security.zipsigner.ZipSigner;
+
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.net.Uri;
 import android.util.Log;
 import com.android.sdklib.build.*;
@@ -72,77 +75,60 @@ public class APKPackager  extends CordovaPlugin {
     private void pack(CordovaArgs args, CallbackContext callbackContext) {
 	File playground = null;
 	File template = null;
-	File assets = null;
+        Uri srcTemplateUri = null;
+	File wwwDir = null;
         File output = null;
-	File zip = null;
-	File archive = null;
-	File publicKey = null;
-	File privateKey = null;
-	File appJSON = null;
-    	URL publicKeyUrl=null;
-    	URL privateKeyUrl=null;
-	
-	try {
-	} catch (Exception e) {
-            callbackContext.error("Error on cleaning: "+e.getMessage());
-            return;
-	}
+        URL keyStoreUrl = null;
+        char[] keyStorePassword = null;
+        String keyAlias = null;
+        char[] keyPassword = null;
+        String appName = "";
+        String packageName = "";
+        String versionName = "";
+        long versionCode = 0; // This is actually an unsigned int32.
 
+        CordovaResourceApi cra = webView.getResourceApi();
 	try {
-  	  CordovaResourceApi cra = webView.getResourceApi();
           playground = cra.mapUriToFile(cra.remapUri(Uri.parse(args.getString(0))));
-          assets = cra.mapUriToFile(cra.remapUri(Uri.parse(args.getString(2))));
+          srcTemplateUri = cra.remapUri(Uri.parse(args.getString(1)));
+        wwwDir = cra.mapUriToFile(cra.remapUri(Uri.parse(args.getString(2))));
           output = cra.mapUriToFile(cra.remapUri(Uri.parse(args.getString(3))));
-          zip = cra.mapUriToFile(cra.remapUri(Uri.parse(args.getString(1))));
-	  archive = new File(playground, "archive");
-	  template = new File(playground, "template");
+        keyStoreUrl = new URL(cra.remapUri(Uri.parse(args.getString(4))).toString());
+        keyStorePassword = args.getString(5).toCharArray();
+        keyAlias = args.getString(6);
+        keyPassword = args.getString(7).toCharArray();
+        JSONObject jObject = args.getJSONObject(8);
+        appName = jObject.getString("appName");
+        packageName = jObject.getString("packageName");
+        versionName = jObject.getString("versionName");
+        versionCode = jObject.getLong("versionCode");
+
+        template = new File(playground, "template");
 	} catch (Exception e) {
+        e.printStackTrace();
             callbackContext.error("Missing arguments: "+e.getMessage());
             return;
 	}
 
-	try {
-          deleteDir(archive);
-	  extractToFolder(zip, archive);	    
-	} catch (Exception e) {
-            callbackContext.error("Error at unzip: "+e.getMessage());
+        try {
+            deleteDir(playground);
+            output.delete();
+            initTemplate(cra, cordova.getActivity().getAssets(), srcTemplateUri, template);
+        } catch (IOException e) {
+            e.printStackTrace();
+            callbackContext.error(e.getMessage());
             return;
-	}
-
-	String appName = "";
-	String packageName = "";
-	String versionName = "";
-	String privateKeyPass = "";
-	String publicKeyName = "";
-	String privateKeyName = "";
-	try {
-	  appJSON = new File(archive, "app.json");
-	  String jsonString = loadJSONFromFile(appJSON);
-	  JSONObject jObject = new JSONObject(jsonString);	
-	  appName = jObject.getString("appName");
-	  packageName = jObject.getString("packageName");
-	  versionName = jObject.getString("versionName");
-	  privateKeyPass = jObject.getString("keyPassword");
-	  publicKeyName = jObject.getString("publicKeyName");
-	  privateKeyName = jObject.getString("privateKeyName");
-
-	  publicKey = new File(archive, publicKeyName);
-	  privateKey = new File(archive, privateKeyName);
-	  publicKeyUrl = publicKey.toURI().toURL();
-	  privateKeyUrl = privateKey.toURI().toURL();
-	} catch (Exception e) {
-            callbackContext.error("Error at parsing the JSON: "+e.getMessage());
-            return;
-	}
+        }
 
 
-	try {
+        try {
 	  BinaryXMLParser parser = new BinaryXMLParser(template.getAbsolutePath()+"/AndroidManifest.xml");
   	  parser.parseXML();
 	  parser.changeString(parser.getAppName(), appName);
 	  parser.changeString(parser.getPackageName(), packageName);
 	  parser.changeString(parser.getActivityName(), appName);
 	  parser.changeString(parser.getVersion(), versionName);
+        // TODO: set versionCode here.
   	  parser.exportXML(template.getAbsolutePath()+"/AndroidManifest.xml");
 
 	  BinaryResourceParser resParser = new BinaryResourceParser(template.getAbsolutePath()+"/resources.arsc");
@@ -155,17 +141,13 @@ public class APKPackager  extends CordovaPlugin {
 	    callbackContext.error("Error at modifing the Android Manifest: "+e.getMessage());
 	}
 
-        String generatedApkPath = playground.getAbsolutePath()+"/temp.apk";
-        String signedApkPath=output.getAbsolutePath()+"/"+appName+".apk";
+        String generatedApkPath = playground.getAbsolutePath()+"/temp-unsigned.apk";
 
 
 	//TODO: to copy the assets directory in the template
 	try {
-	    File destAssets = new File(template, "assets");
-	    File pluginAssets = new File(playground, "plugins");
-	    deleteDirContent(destAssets);
-	    mergeDirectory(assets, destAssets);
-	    mergeDirectory(pluginAssets, destAssets);
+	    File destWww = new File(new File(template, "assets"), "www");
+	    mergeDirectory(cra, wwwDir, destWww);
 	} catch (Exception e) {
             callbackContext.error("Error at assets copy: "+e.getMessage());
             return;
@@ -179,8 +161,7 @@ public class APKPackager  extends CordovaPlugin {
             fakeResZip = new File(playground,"FakeResourceZipFile.zip");
             writeZipfile(fakeResZip);
 
-            ApkBuilder b = new ApkBuilder(generatedApkPath,fakeResZip.getPath(),
-					  playground.getAbsolutePath()+"/classes.dex",null,null,null);
+            ApkBuilder b = new ApkBuilder(generatedApkPath,fakeResZip.getPath(), null,null,null,null);
 	    b.addSourceFolder(template);
             b.sealApk();
         } catch (Exception e) {
@@ -191,10 +172,7 @@ public class APKPackager  extends CordovaPlugin {
         // sign the APK with the supplied key/cert
         try {
             ZipSigner zipSigner = new ZipSigner();
-            X509Certificate cert = zipSigner.readPublicKey(publicKeyUrl);
-            PrivateKey pk = zipSigner.readPrivateKey(privateKeyUrl,  privateKeyPass);
-            zipSigner.setKeys("xx", cert, pk, null);
-            zipSigner.signZip(generatedApkPath, signedApkPath);
+            zipSigner.signZip(keyStoreUrl, null, keyStorePassword, keyAlias, keyPassword, "RSA", generatedApkPath, output.getAbsolutePath());
         } catch (Exception e) {
             callbackContext.error("ZipSigner Error: "+e.getMessage());
             return;
@@ -204,13 +182,47 @@ public class APKPackager  extends CordovaPlugin {
         try {
             new File(generatedApkPath).delete();
 	    fakeResZip.delete();
-	    deleteDir(archive);
         } catch (Exception e) {
             callbackContext.error("Error cleaning up: "+e.getMessage());
             return;
 	}
 
         callbackContext.success("succes for " + appName);
+    }
+
+    private static String join(String a, String b) {
+        if (a.endsWith("/")) {
+            a = a.substring(0, a.length() - 1);
+        }
+        if (b.startsWith("/")) {
+            b = b.substring(1);
+        }
+        return a + '/' + b;
+    }
+
+    private static void mergeFromAssets(CordovaResourceApi cra, AssetManager assetManager, String from, Uri to) throws IOException {
+        String[] assets = assetManager.list(from);
+        if (assets.length == 0) {
+            cra.copyResource(Uri.parse(join("file:///android_asset/", from)), to);
+        } else {
+            File f = new File(to.getPath());
+            f.mkdirs();
+            for (String a : assets) {
+                mergeFromAssets(cra, assetManager, join(from, a), Uri.parse(join(to.toString(), a)));
+            }
+        }
+    }
+
+    private static void initTemplate(CordovaResourceApi cra, AssetManager assetManager, Uri srcTemplateUri, File dstTemplate) throws IOException {
+        File f = cra.mapUriToFile(srcTemplateUri);
+        if (f != null) {
+            mergeDirectory(cra, f, dstTemplate);
+        } else if (srcTemplateUri.toString().startsWith("file:///android_asset/")) {
+            String assetPath = srcTemplateUri.getPath().substring("/android_asset/".length());
+            mergeFromAssets(cra, assetManager, assetPath, Uri.fromFile(dstTemplate));
+        } else {
+            throw new UnsupportedOperationException("Cannot handle URL: " + srcTemplateUri);
+        }
     }
 
     private void deleteDir(File dir){
@@ -234,19 +246,6 @@ public class APKPackager  extends CordovaPlugin {
         out.closeEntry();
         out.close();
     }
-    
-    private void deleteDirContent(File dir) {
-        if(!dir.exists()) return;
-        if(dir.isDirectory()) {
-            File [] files = dir.listFiles();
-            if(files != null) {
-                for( File f : files ) {
-    	            if(f.isDirectory()) deleteDir(f);
-    	            else f.delete();
-                }
-            }
-        }
-    }
 
     private void writeStringToFile(String str, File target) {
     	FileWriter fw=null;
@@ -266,96 +265,18 @@ public class APKPackager  extends CordovaPlugin {
     
     /* overwrite stuff from a default zip with the things in sourcedir 
     */
-    private void mergeDirectory(File srcdir, File workdir) 
+    private static void mergeDirectory(CordovaResourceApi cra, File srcdir, File workdir)
             throws FileNotFoundException, IOException {
         File[] files = srcdir.listFiles();
         for(File file : files){
             if(file.isDirectory()) {
                 File targetDir = new File(workdir, file.getName());
                 targetDir.mkdirs();
-                mergeDirectory(file, targetDir);
+                mergeDirectory(cra, file, targetDir);
             } else {
                 File targetFile = new File(workdir, file.getName());
                 if(targetFile.exists()) targetFile.delete();
-                copyFile(file, targetFile);
-            }
-        }
-    }
-
-    private void copyFile(File src, File dest)
-            throws FileNotFoundException, IOException {
-        FileInputStream istream = new FileInputStream(src);
-        FileOutputStream ostream = new FileOutputStream(dest);
-        FileChannel input = istream.getChannel();
-        FileChannel output = ostream.getChannel();
-
-        try {
-            input.transferTo(0, input.size(), output);
-        } finally {
-            istream.close();
-            ostream.close();
-            input.close();
-            output.close();
-        }
-    }
-
-  public String loadJSONFromFile(File file) {
-    String json = null;
-    try {
-        InputStream is = new BufferedInputStream(new FileInputStream(file));
-        int size = is.available();
-        byte[] buffer = new byte[size];
-        is.read(buffer);
-        is.close();
-        json = new String(buffer, "UTF-8");
-    } catch (IOException ex) {
-        ex.printStackTrace();
-        return null;
-    }
-    return json;
-  }
-
-    private void extractToFolder(File zipfile, File tempdir) {
-    	InputStream inputStream=null;
-    	try {
-            FileInputStream zipStream = new FileInputStream(zipfile);
-            inputStream = new BufferedInputStream(zipStream);
-            ZipInputStream zis = new ZipInputStream(inputStream);
-            inputStream = zis;
-
-            ZipEntry ze;
-            byte[] buffer = new byte[32 * 1024];
-
-            while ((ze = zis.getNextEntry()) != null)
-            {
-                String compressedName = ze.getName();
-
-                if (ze.isDirectory()) {
-                   File dir = new File(tempdir, compressedName);
-                   dir.mkdirs();
-                } else {
-                    File file = new File(tempdir, compressedName);
-                    file.getParentFile().mkdirs();
-                    if(file.exists() || file.createNewFile()){
-                        FileOutputStream fout = new FileOutputStream(file);
-                        int count;
-                        while ((count = zis.read(buffer)) != -1)
-                        {
-                            fout.write(buffer, 0, count);
-                        }
-                        fout.close();
-                    }
-                }
-                zis.closeEntry();
-            }
-        } catch (Exception e) {
-
-        } finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                }
+                cra.copyResource(Uri.fromFile(file), Uri.fromFile(targetFile));
             }
         }
     }
